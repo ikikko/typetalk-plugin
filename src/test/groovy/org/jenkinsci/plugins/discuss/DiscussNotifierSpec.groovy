d0964f5bceb50306080f3cc5b93979b0105cc83e
nakamura <nakamura@nulab.co.jp>
2013-03-10 01:56:01 +0900

    spockテストコードの体裁を整える

--- src/test/groovy/org/jenkinsci/plugins/discuss/DiscussNotifierSpec.groovy
+++ src/test/groovy/org/jenkinsci/plugins/discuss/DiscussNotifierSpec.groovy
@@ -3,9 +3,11 @@ package org.jenkinsci.plugins.discuss
 import hudson.model.AbstractBuild
 import hudson.model.Result
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class DiscussNotifierSpec extends Specification {
 
+	@Unroll
 	def "各種条件に応じた toBuildSummary のテストを行う"() {
 		setup:
 		def notifier = new DiscussNotifier(notifyWhenSuccess)
@@ -15,26 +17,26 @@ class DiscussNotifierSpec extends Specification {
 		expected == notifier.toBuildSummary(build)
 
 		where:
-		notifyWhenSuccess | previousResult | result          | expected
-		false             | Result.SUCCESS | Result.SUCCESS  | null
-		false             | Result.SUCCESS | Result.UNSTABLE | 'Build unstable.'
-		false             | Result.SUCCESS | Result.FAILURE  | 'Build failure.'
-		false             | Result.SUCCESS | Result.ABORTED  | 'Build aborted.'
+		notifyWhenSuccess | previousResult | result          || expected
+		false             | Result.SUCCESS | Result.SUCCESS  || null
+		false             | Result.SUCCESS | Result.UNSTABLE || 'Build unstable.'
+		false             | Result.SUCCESS | Result.FAILURE  || 'Build failure.'
+		false             | Result.SUCCESS | Result.ABORTED  || 'Build aborted.'
 
-		false             | Result.FAILURE | Result.SUCCESS  | 'Build recovered.'
-		false             | Result.FAILURE | Result.UNSTABLE | 'Build unstable.'
-		false             | Result.FAILURE | Result.FAILURE  | 'Build failure.'
-		false             | Result.FAILURE | Result.ABORTED  | 'Build aborted.'
+		false             | Result.FAILURE | Result.SUCCESS  || 'Build recovered.'
+		false             | Result.FAILURE | Result.UNSTABLE || 'Build unstable.'
+		false             | Result.FAILURE | Result.FAILURE  || 'Build failure.'
+		false             | Result.FAILURE | Result.ABORTED  || 'Build aborted.'
 
-		true              | Result.SUCCESS | Result.SUCCESS  | 'Build successful.'
-		true              | Result.SUCCESS | Result.UNSTABLE | 'Build unstable.'
-		true              | Result.SUCCESS | Result.FAILURE  | 'Build failure.'
-		true              | Result.SUCCESS | Result.ABORTED  | 'Build aborted.'
+		true              | Result.SUCCESS | Result.SUCCESS  || 'Build successful.'
+		true              | Result.SUCCESS | Result.UNSTABLE || 'Build unstable.'
+		true              | Result.SUCCESS | Result.FAILURE  || 'Build failure.'
+		true              | Result.SUCCESS | Result.ABORTED  || 'Build aborted.'
 
-		true              | Result.FAILURE | Result.SUCCESS  | 'Build recovered.'
-		true              | Result.FAILURE | Result.UNSTABLE | 'Build unstable.'
-		true              | Result.FAILURE | Result.FAILURE  | 'Build failure.'
-		true              | Result.FAILURE | Result.ABORTED  | 'Build aborted.'
+		true              | Result.FAILURE | Result.SUCCESS  || 'Build recovered.'
+		true              | Result.FAILURE | Result.UNSTABLE || 'Build unstable.'
+		true              | Result.FAILURE | Result.FAILURE  || 'Build failure.'
+		true              | Result.FAILURE | Result.ABORTED  || 'Build aborted.'
 	}
 
 	def makeMockBuild(Result result, Result previousResult) {
