dd46452639bc6df296aa5ac4e1696b9a795961bf
nakamura <nakamura@nulab.co.jp>
2013-05-18 23:11:50 +0900

    Not post in test method.

--- src/test/java/org/jenkinsci/plugins/typetalk/TypetalkTest.java
+++ src/test/java/org/jenkinsci/plugins/typetalk/TypetalkTest.java
@@ -1,14 +1,19 @@
 package org.jenkinsci.plugins.typetalk;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TypetalkTest {
+
+	// TODO APIキーを埋め込まない
+	// 念のために、公開リリース前は新しいキーに変更して、既存のキーを使ってるところはreplaceしておくこと
 
 	// Jenkins 君
 	private static final String API_KEY = "QWBQZUGleHTBFIC3CmMfAD91Kj68QL9h";
 
 	@Test
-	public void test() throws Exception {
+	@Ignore("not post usually")
+	public void postMessage() throws Exception {
 		Typetalk typetalk = new Typetalk(API_KEY);
 		typetalk.postMessage(9L, "test"); // topic: Labs
 	}
